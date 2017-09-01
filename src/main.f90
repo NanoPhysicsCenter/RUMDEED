9494f7953dde4bb3dd0f4e54e442a48f6ddbf86f
Kristinn Torfason <kristinnt@ru.is>
2017-09-01 11:14:16 +0000

    On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	new file:   build/makefile
    	new file:   data/input
    	new file:   makefile
    	renamed:    main.f90 -> src/main.f90
    	new file:   src/mod_global.f90

--- src/main.f90
+++ src/main.f90
@@ -1,5 +1,7 @@
 program Vacuum
+  use mod_global
   implicit none
 
   print *, 'Hi'
+  print *, q_e
 end program Vacuum
---
+++ src/mod_global.f90
@@ -0,0 +1,4 @@
+module mod_global
+  double precision, parameter :: q_e = 1.602d-19
+contains
+end module mod_global
