790a0e8c683d6dbf5e5bc07118a76bc1cc8545b1
Kristinn Torfason <kristinnt@ru.is>
2018-06-23 16:36:27 +0000

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   doc/.gitignore
    	modified:   src/mod_field_emission_v2.f90

--- src/mod_field_emission_v2.f90
+++ src/mod_field_emission_v2.f90
@@ -489,6 +489,84 @@ contains
   end function Escape_Prob
 
   !-----------------------------------------------------------------------------
+  ! Metropolis-Hastings algorithm
+  ! Includes that the work function can vary with position
+  function Metropolis_Hastings_rectangle_v2(ndim, emit)
+    integer, intent(in)              :: ndim, emit
+    double precision, dimension(1:3) :: Metropolis_Hastings_rectangle_v2
+    integer                          :: count, i
+    double precision                 :: std, rnd, alpha
+    double precision, dimension(1:3) :: cur_pos, new_pos, field
+    double precision                 :: df_cur, df_new
+
+    std = (emitters_dim(1, emit)*0.05d0 + emitters_dim(2, emit)*0.05d0) / (2.0d0)
+
+    ! Get a random position on the surface
+    count = 0
+    do ! Infinite loop
+      CALL RANDOM_NUMBER(cur_pos(1:2))
+      cur_pos(1:2) = cur_pos(1:2)*emitters_dim(1:2, emit) + emitters_pos(1:2, emit)
+      cur_pos(3) = 0.0d0 ! On the surface
+
+      ! Calculate the electric field at this position
+      field = Calc_Field_at(cur_pos)
+      if (field(3) < 0.0d0) then
+        exit ! The loop is infinite
+      else
+        count = count + 1
+        if (count > 1000) exit ! The loop is infnite, must stop it at some point
+      end if
+    end do
+
+    ! Calculate the escape probability at this location
+    if (field(3) < 0.0d0) then
+      df_cur = Escape_Prob(field(3), cur_pos)
+    else
+      df_cur = 0.0d0 ! Zero escape probabilty if field is not favourable
+    end if
+
+    !---------------------------------------------------------------------------
+    ! We now pick a random distance and direction to jump to from our
+    ! current location. We do this ndim times.
+    do i = 1, ndim
+      ! Find a new position
+      new_pos(1:2) = cur_pos(1:2) + box_muller(0.0d0, std)
+
+      ! Calculate the field at the new position
+      field = Calc_Field_at(new_pos)
+
+      ! Check if the field is favourable for emission at the new position.
+      ! If it is not then cycle, i.e. we reject this location and
+      ! pick another one.
+      if (field(3) > 0.0d0) cycle ! Do the next loop iteration
+
+      ! Calculate the escape probability at the new position, to compair with
+      ! the current position.
+      df_new = Escape_Prob(field(3), new_pos)
+
+      ! If the escape probability is higher in the new location,
+      ! then we jump to that location. If it is not then we jump to that
+      ! location with the probabilty df_new / df_cur.
+      if (df_new > df_cur) then
+        cur_pos = new_pos ! New position becomes the current position
+        df_cur = df_new
+      else
+        alpha = df_new / df_cur
+
+        CALL RANDOM_NUMBER(rnd)
+        if (rnd < alpha) then
+          cur_pos = new_pos ! New position becomes the current position
+          df_cur = df_new
+        end if
+      end if
+    end do
+
+    ! Return the current position
+    Metropolis_Hastings_rectangle_v2(1:2) = cur_pos(1:2)
+    Metropolis_Hastings_rectangle_v2(3) = df_cur
+  end function Metropolis_Hastings_rectangle_v2
+
+  !-----------------------------------------------------------------------------
   ! Metropolis–Hastings algorithm
   ! https://en.wikipedia.org/wiki/Metropolis%E2%80%93Hastings_algorithm
   function Metro_algo_rec(ndim, emit)
@@ -504,6 +582,7 @@ contains
     CALL RANDOM_NUMBER(par_pos)
 
     par_pos(1:2) = par_pos(1:2)*emitters_dim(1:2, emit) + emitters_pos(1:2, emit)
+    par_pos(3) = 0.0d0
 
     field = Calc_Field_at(par_pos)*(-1.0d0) ! This code assumes it is using the acceleration
 
