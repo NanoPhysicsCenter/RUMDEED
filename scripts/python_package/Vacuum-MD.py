52e684525a085c2615529b479f3c6d65232f7d5c
Kristinn Torfason <kristinnt@ru.is>
2018-11-11 17:53:09 +0000

    Changes to be committed: 	modified:   scripts/python_package/Vacuum-MD.py

--- scripts/python_package/Vacuum-MD.py
+++ scripts/python_package/Vacuum-MD.py
@@ -1,3 +1,6 @@
+import numpy as np
+import pandas as pd
+
 # ----------------------------------------------------------------
 # Calculates the emittance
 # Input is a pandas dataframe that has columns called x and x'.
