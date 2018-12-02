ab3f6c0c85977db5f8a93857d7571b8d76b0e947
Kristinn Torfason <kristinnt@ru.is>
2018-12-02 14:18:27 +0000

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   src/mod_photo_emission.f90

--- src/mod_photo_emission.f90
+++ src/mod_photo_emission.f90
@@ -129,7 +129,7 @@ contains
         par_vel = 0.0d0 ! Set the velocity
 
         ! Escape velocity from image charge partner
-        par_vel(3) = ( q_0 / sqrt(8*pi*epsilon_0*epsilon_r*m_0*par_pos(3))) )*0.80d0
+        par_vel(3) = ( q_0 / sqrt(8.0d0*pi*epsilon_0*epsilon_r*m_0*par_pos(3)) )*0.80d0
         
         ! Speed needed to reach over the gap spacing. Includes image charge partners behind the cathode and annode but not the electric field.
         !par_vel(3) = q_0/(4.0d0*sqrt(pi*epsilon_0*epsilon_r*m_0))*(d-2.0d0*par_pos(3))/sqrt(d*par_pos(3)*(d-par_pos(3)))
