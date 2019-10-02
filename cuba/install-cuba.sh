4030856247f1e9d80445a497474e61bbb3e7f2b1
Kristinn Torfason <kristinnt@ru.is>
2019-10-02 22:12:40 +0000

    On branch dev  Changes to be committed: 	modified:   build/makefile 	modified:   cuba/install-cuba.sh

--- cuba/install-cuba.sh
+++ cuba/install-cuba.sh
@@ -2,7 +2,7 @@
 
 installdir=$PWD
 cd 4.2
-./configure --prefix="${installdir// /\\ }" CC=gcc-6 FC=gfortran-6
+./configure --prefix="${installdir// /\\ }" CC=gcc FC=gfortran
 make clean
 make
 make install
