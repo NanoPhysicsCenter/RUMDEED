1f2c74c8656958bc22f043172dc57233c8f8ce0d
Kristinn Torfason <kristinnt@ru.is>
2020-04-02 11:00:05 +0000

    On branch dev  Your branch is up to date with 'origin/dev'.
     Changes to be committed:
    	modified:   scripts/python_package/Calc_EB.py

--- scripts/python_package/Calc_EB.py
+++ scripts/python_package/Calc_EB.py
@@ -6,6 +6,8 @@ import numpy as np
 import pandas as pd
 import Vacuum
 import os.path as path
+import matplotlib.pyplot as plt
+from matplotlib.patches import Ellipse
 
 filepath = './'
 filename = path.join(filepath, 'density_absorb_top.bin')
@@ -42,6 +44,10 @@ df["r'"] = (np.sqrt(df['vx']**2 + df['vy']**2) / df['vz']) / 1.0E-3
 e_x, sw, swp, th = Vacuum.Calc_Emittance(df, "x", "x'")
 e_y, _, _, _ = Vacuum.Calc_Emittance(df, "y", "y'")
 
+sigma_w = sw
+sigma_wp = swp
+theta_ell = th
+
 print('Emittance in x-direction {:6.2f} nm-mrad'.format(e_x))
 print('Emittance in y-direction {:6.2f} nm-mrad'.format(e_y))
 
@@ -58,3 +64,33 @@ print('Current is {:6.2f} mA'.format(cur/1.0E-3))
 # Calculate brightness
 B = 2*cur/(np.pi**2*e_x*e_y * 1.0E-12)
 print('Brightness is {:6.2f} A/(m-rad)^2'.format(B))
+
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif', size=8)
+#plt.rcParams['figure.figsize'] = [10, 7.5]
+#plt.rcParams['figure.figsize'] = [2.75, 2.0625]
+plt.rc('xtick', labelsize=8)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=8)    # fontsize of the tick labels
+plt.rc('axes', labelsize=8)    # fontsize of the x and y labels
+
+#plt.rcParams['axes.linewidth'] = 5
+
+ell_0 = Ellipse([0.0, 0.0], 2.0*sigma_w, 2.0*sigma_wp, theta_ell, zorder=1, linewidth=0.25)
+ax = plt.subplot(111)
+
+for axis in ['top','bottom','left','right']:
+  ax.spines[axis].set_linewidth(0.25)
+
+ax.grid(linestyle='-', linewidth='0.25')
+
+ax.plot(df['x'], df["x'"], 'o', marker='o', markersize=2.5, zorder=0)
+plt.xlabel('$x$ [nm]')
+plt.ylabel(r'$x^\prime$ [mrad]')
+ax.add_artist(ell_0)
+ell_0.set_facecolor('red')
+ell_0.set_alpha(0.5)
+
+plt.rcParams['savefig.pad_inches'] = 0.0
+#plt.savefig('emittance.png', bbox_inches='tight', transparent=True, dpi=600)
+
+plt.show()
