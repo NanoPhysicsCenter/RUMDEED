8ba72555f02ed9cf111f1cbbf9aeb878a6256618
Kristinn Torfason <kristinnt@ru.is>
2021-01-25 13:28:56 +0000

    On branch dev  Your branch is up to date with 'origin/dev'.
     Changes to be committed:
    	modified:   cuba/install-cuba.sh

--- cuba/install-cuba.sh
+++ cuba/install-cuba.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 installdir=$PWD
-cd 4.2
-./configure --prefix="${installdir// /\\ }" CC=gcc FC=gfortran
+cd 4.2.1
+./configure --prefix="${installdir// /\\ }" CC=gcc-10 FC=gfortran-10
 make clean
 make -j8
 make install
