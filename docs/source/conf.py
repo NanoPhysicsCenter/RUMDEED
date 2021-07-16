0628fa784acd0dd85ca1f187040d11f3de95a89b
Kristinn Torfason <kristinnt@ru.is>
2021-07-16 16:48:49 +0000

    Sphinx docs added

---
+++ docs/source/conf.py
@@ -0,0 +1,69 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'Vacuum-MD'
+copyright = '2021, Kristinn Torfason'
+author = 'Kristinn Torfason'
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+        'sphinxfortran.fortran_domain',
+        'sphinxfortran.fortran_autodoc',
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'sphinx_rtd_theme'
+#html_theme = 'alabaster'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+
+# -- Math - KT
+numfig = True
+math_numfig = True
+numfig_secnum_depth = 2
+math_eqref_format = "Eq. {number}"
+
+def setup(app):
+    app.add_css_file('css/custom.css')
+
+# -- Fortran code
+fortran_src = [
+        '../src/main.f90',
+        ]
---
+++ docs/source/index.rst
@@ -0,0 +1,47 @@
+.. Vacuum-MD documentation master file, created by
+   sphinx-quickstart on Fri Jul 16 14:24:50 2021.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Vacuum-MD's documentation!
+=====================================
+
+**Lumache** (/lu'make/) is a Python library for cooks and food lovers that
+creates recipes mixing random ingredients.  It pulls data from the `Open Food
+Facts database <https://world.openfoodfacts.org/>`_ and offers a *simple* and
+*intuitive* API.
+
+Check out the :doc:`usage` section for further information.
+:ref:`install <installation>` the project.
+
+Since Pythagoras, we know that :math:`a^2 + b^2 = c^2`.
+
+.. math::
+    e^{i\pi} + 1 = 0
+    :label: eq:euler
+
+Euler's identity, equation :math:numref:`euler`, was elected one of the
+most beautiful mathematical formulas.
+
+I like birds
+
+.. f:autoprogram:: Awesome
+
+.. note::
+
+   This project is under active development.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   usage
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
