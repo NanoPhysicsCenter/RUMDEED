23080d0a197e5ead2fc64a41b2cd6d46c1c9757e
Kristinn Torfason <kristinnt@ru.is>
2021-08-24 18:08:03 +0000

    Docs updated

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '640e26d53a2c755bb82925f924cd7d31eea1a9e9',
+    VERSION: '',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
