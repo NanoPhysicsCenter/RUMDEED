a1df82c461546ba460f8469a5282c1bbeda2d3ee
Kristinn Torfason <kristinnt@ru.is>
2021-09-01 12:38:11 +0000

    Cuba install script updated

--- cuba/install-cuba.sh
+++ cuba/install-cuba.sh
@@ -2,9 +2,4 @@
 
 installdir=$PWD
 cd 4.2.1
-./configure --prefix="${installdir// /\\ }" CC=$1 FC=$2
-make clean
-make lib -j 8
-make install
-make clean
-#rm config.h
+./configure --prefix="${installdir// /\\ }" CC=$1 FC=$2 && make clean && make lib -j 8 && make install && make clean
