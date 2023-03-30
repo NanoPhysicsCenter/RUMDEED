6e939e15bf6d3ebed82a4e2862ad4c0e9935d782
Kristinn Torfason <kristinnt@ru.is>
2023-03-30 14:17:20 +0000

    Program will now stop on SIGUSR1

--- src/main.F90
+++ src/main.F90
@@ -426,7 +426,8 @@ contains
 
     ! Register a subroutine to catch the SIGINT signal
 #if defined(__GNUC__)
-    IFAIL = SIGNAL(SIGINT, Signal_Handler)
+    IFAIL = SIGNAL(SIGINT, Signal_Handler) ! SIGINT is used by to stop the simulations by ctrl+c
+    IFAIL = SIGNAL(16, Signal_Handler) ! SIGUSR1 is used by slurm to stop the simulation
 #endif
 
     ! Create folder for output files
