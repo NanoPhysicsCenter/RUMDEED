422bd7e2cc0ee95f9b92e11292f3c7bf2ff82258
Kristinn Torfason <kristinnt@ru.is>
2023-04-25 11:55:58 +0000

    GitHub pages updated

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,12 +1,14 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0a10c506c991fd6a860b65a05e622270dc47809e',
-    LANGUAGE: 'None',
+    VERSION: '',
+    LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
     LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
-    NAVIGATION_WITH_KEYS: false
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
 };
