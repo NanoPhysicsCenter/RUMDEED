ea14432972c6d247d9a70cf299080b072c32bfd6
4rnar <arnarjons7@gmail.com>
2025-03-03 21:41:21 +0000

    changed emit_ring to 4

--- src/mod_global.F90
+++ src/mod_global.F90
@@ -229,9 +229,9 @@ module mod_global
   ! Emitter types
   integer, parameter :: EMIT_UNKNOWN         = 0
   integer, parameter :: EMIT_CIRCLE          = 1
-  integer, parameter :: EMIT_RING            = 2
   integer, parameter :: EMIT_RECTANGLE       = 2
   integer, parameter :: EMIT_RECTANGLE_SPOTS = 3
+  integer, parameter :: EMIT_RING            = 4
 
 
   double precision, dimension(:), allocatable :: ramo_current
